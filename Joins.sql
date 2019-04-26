2b5d1ca6adb3a12932c4a4c9b6bb705edf6e406a
Evelyn <evc3355@gmail.com>
2019-04-26 20:56:18 +0100

    Update Joins.sql

--- Joins.sql
+++ Joins.sql
@@ -1,5 +1,5 @@
-Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
-Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+/*Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.*/
 
 select sum(city.population)
 from city
@@ -8,8 +8,8 @@ on city.countrycode = country.code
 where country.continent = "Asia";
 
 
-Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
-Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+/*Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.*/
 
 select city.name 
 from city
@@ -18,8 +18,8 @@ on city.countrycode = country.code
 where country.continent = "Africa";
 
 
-Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
-Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+/*Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.*/
 
 select distinct country.continent, avg(city.population)
 from city
