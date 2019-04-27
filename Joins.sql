effab38404a1141351b5279ef8a2479aed092f64
Evelyn <evc3355@gmail.com>
2019-04-27 15:13:01 +0100

    Update Joins.sql

--- Joins.sql
+++ Joins.sql
@@ -41,3 +41,9 @@ from station;
 select truncate(sum(lat_n),4) 
 from station 
 where lat_n between 38.788 and 137.2345;
+
+/* What is the longitude of the northernmost station below latitude 137 8/
+select top 1 round(long_w,4) 
+from station 
+where lat_n < 137.2345 
+order by lat_n desc;
