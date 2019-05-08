ab885753fe3d3f03572fc4dd410317007ec45e5f
Evelyn <evc3355@gmail.com>
2019-05-08 08:55:45 +0100

    Update Basic.sql

--- Basic.sql
+++ Basic.sql
@@ -14,10 +14,10 @@ FROM station
 WHERE lat_n BETWEEN 38.788 AND 137.2345;
 
 /* What is the longitude of the northernmost station below latitude 137 */
-select top 1 round(long_w,4) 
-from station 
-where lat_n < 137.2345 
-order by lat_n desc;
+SELECT TOP 1 ROUND(long_w,4) 
+FROM station 
+WHERE lat_n < 137.2345 
+ORDER BY lat_n DESC;
 
 SELECT TOP 1 CAST(LONG_W AS DECIMAL (10,4)) FROM STATION
 WHERE LAT_N > 38.778
