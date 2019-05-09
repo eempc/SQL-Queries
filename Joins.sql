780a53738eb3dd4a2df9ce73ab1bc9a4b3a4c83c
Evelyn <evc3355@gmail.com>
2019-05-09 15:23:11 +0100

    Update Joins.sql

--- Joins.sql
+++ Joins.sql
@@ -1,11 +1,11 @@
 /*Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
 Note: CITY.CountryCode and COUNTRY.Code are matching key columns.*/
 
-select sum(city.population)
-from city
-join country
-on city.countrycode = country.code
-where country.continent = "Asia";
+SELECT SUM(city.population)
+FROM city
+JOIN country
+ON city.countrycode = country.code
+WHERE country.continent = "Asia";
 
 
 /*Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
