17630773f57dd08c2a9babffe0385d317e566838
Evelyn <evc3355@gmail.com>
2019-05-10 08:38:20 +0100

    Update Joins.sql

--- Joins.sql
+++ Joins.sql
@@ -11,11 +11,11 @@ WHERE country.continent = "Asia";
 /*Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
 Note: CITY.CountryCode and COUNTRY.Code are matching key columns.*/
 
-select city.name 
-from city
-join country
-on city.countrycode = country.code
-where country.continent = "Africa";
+SELECT city.name 
+FROM city
+JOIN country
+ON city.countrycode = country.code
+WHERE country.continent = "Africa";
 
 
 /*Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
