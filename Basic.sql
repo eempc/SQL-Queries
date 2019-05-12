249ad6df87b60ecb67e937007dea3a7d417d208f
Evelyn <evc3355@gmail.com>
2019-05-12 08:12:28 +0100

    Update Basic.sql

--- Basic.sql
+++ Basic.sql
@@ -1,6 +1,6 @@
 /* Strange queries */
 SELECT TOP 1 salary * months, COUNT(*) 
-FROM employee 
+FROM Employee 
 GROUP BY salary * months 
 ORDER BY salary * months DESC;
 
@@ -10,15 +10,16 @@ FROM station;
 
 /* Like Excel's =sumif() */
 SELECT TRUNCATE(SUM(lat_n),4) 
-FROM station 
+FROM Station 
 WHERE lat_n BETWEEN 38.788 AND 137.2345;
 
 /* What is the longitude of the northernmost station below latitude 137 */
 SELECT TOP 1 ROUND(long_w,4) 
-FROM station 
+FROM Station 
 WHERE lat_n < 137.2345 
 ORDER BY lat_n DESC;
 
-SELECT TOP 1 CAST(LONG_W AS DECIMAL (10,4)) FROM STATION
-WHERE LAT_N > 38.778
-ORDER BY LAT_N ASC;
+SELECT TOP 1 CAST(long_w AS DECIMAL (10,4)) 
+FROM Station
+WHERE lat_n > 38.778
+ORDER BY lat_n ASC;
